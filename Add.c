4523fc6457dba2cca3a7d26ba6e295aed1e70cfb
Harsha <hkoganti@my.okcu.edu>
2015-03-12 18:28:30 -0500

    basic program to perform addition

---
+++ Add.c
@@ -0,0 +1,15 @@
+
+#include<stdio.h>
+void main()
+{
+int num1,num2;
+float result;
+printf("Enter the num1\n");
+scanf("%d &num1");
+printf("Enter the num2\n");
+scanf("%d &num2");
+result=num1+num2;
+printf("The result is %d",result);
+}
+
+
