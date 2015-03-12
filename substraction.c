2b0f965f1a0a8eda68f15f50e03b6fa01c77489b
surendra <surendrayalavarthi@gmail.com>
2015-03-12 18:27:19 -0500

    basic program to perform substraction

---
+++ substraction.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+void main()
+{
+	int num1,num2,difference;
+	printf("\nEnter the numbers in a way that first number is greater than second number\n");
+	Printf("\nEnter the first number : \n");
+	scaf("%d",&num1);
+	printf("\nEnter the second number : \n");
+	scanf("%d",&num2);
+	difference = num1-num2;
+	printf("The result is %d",difference);
+
+}
